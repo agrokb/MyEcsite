f2ae38eb49f93b037b5a50308d649a96facfe561
larry <dh821020@gmail.com>
2020-05-25 13:21:12 +0900

    Backend index Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'products#index'
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   resource :products,only: [:index,:show]
+
+  namespace :admin do
+    root 'products#index'
+    resources :products
+  end
 end
