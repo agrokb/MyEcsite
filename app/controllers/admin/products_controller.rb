0e25ddc32c29bddfd197b3a3bf9c2ad4f159ea45
larry <dh821020@gmail.com>
2020-06-01 09:16:42 +0900

    Add Backend Product Page Upload Image

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -36,7 +36,15 @@ class Admin::ProductsController < Admin::BaseController
       @product = Product.friendly.find(params[:id])
     end
     def product_params
-     params.require(:product).permit(:name,:vendor_id,:list_price,:sell_price,:on_sell,:description,skus_attributes:[
+     params.require(:product).permit(:name,
+      :vendor_id,
+      :list_price,
+      :sell_price,
+      :on_sell,
+      :cover_image,
+      :category_id,
+      :description,
+      skus_attributes:[
         :id,:spec,:quantity,:_destroy
      ])
     end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,10 +2,12 @@ class Product < ApplicationRecord
   include CodeGenerator
   
   has_rich_text :description
+  has_one_attached :cover_image
 
   validates :name, presence: true
   validates :list_price, :sell_price,presence: true,numericality: { greater_than: 0,allow_nil: true}
   validates :code, uniqueness: true
+  belongs_to :category, optional: true
   belongs_to :vendor
   has_many :skus
   accepts_nested_attributes_for :skus,reject_if: :all_blank,allow_destroy: true
