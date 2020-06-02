7073cbd8b06ad0f7167543033567ba116d717c6b
larry <dh821020@gmail.com>
2020-06-02 15:38:23 +0900

    Add cart and cart_item PORO to TDD

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,9 +1,21 @@
 #PORO
 class Cart
+  attr_reader :items
+  def initialize
+    @items = []
+  end
   def add_item(product_id)
+    found = @items.find{|item| item.product_id == product_id }
+
+    if found 
+      found.increment! 
+    else
+      @items << CartItem.new(product_id)
+    end
   end
 
   def empty?
-    false
-  end
+    @items.empty?
+  end 
+
 end
--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -8,8 +8,16 @@ RSpec.describe Cart, type: :model do
       cart.add_item(2) 
       expect(cart.empty?).to be false
     end
+    it "同じ種類の商品カゴに入れて、購入項目増加さず、商品の数は変更できる" do
+      cart = Cart.new
+      3.times{ cart.add_item(1)}
+      2.times{ cart.add_item(2)} 
+      
+      expect(cart.items.count).to be 2
+      expect(cart.items.first.quantity).to be 3
+    end
   end
-
+  
   context "追加機能" do
   end
 end
