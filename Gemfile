32028462130d96fbd0d5327741265651a699997a
larry <dh821020@gmail.com>
2020-06-07 13:28:00 +0900

    Faraday Gem For API Post

--- Gemfile
+++ Gemfile
@@ -37,6 +37,8 @@ gem 'paranoia', '~> 2.1', '>= 2.1.5'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
 
+gem 'faraday', '~> 1.0', '>= 1.0.1'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,6 +9,20 @@ before_action :authenticate_user!
     end
 
     if @order.save
+      resp = Faraday.post("#{ENV['line_pay_endpoint']}/v2/payments/request") do |req|
+            req.headers['Content-Type'] = 'application/json'
+            req.headers['X-LINE-ChannelId'] = ENV['line_pay_channel_id']
+            req.headers['X-LINE-ChannelSecret'] = ENV['line_pay_channel_secret']
+            req.body = {
+              "productName": "Cake",
+              "amount":current_cart.total_price.to_i,
+              "currency": "JPY",
+              "confirmUrl":"http://localhost:3000/orders/confirm",
+              "orderId": @order.num
+            }.to_json
+      end
+      
+
       redirect_to root_path, notice: 'message'
     else
       render 'carts/checkout'
